d4688d0a0ea23fc5b4db3a59349042b1a38f3dc5
Henrik Olsson <hnrklssn@gmail.com>
2019-08-23 15:32:23 +0100

    add debounce to input

---
+++ src/debounce.rs
@@ -0,0 +1,47 @@
+extern crate timer;
+extern crate chrono;
+use timer::Guard;
+use timer::Timer;
+use chrono::Duration;
+use std::sync::{Arc, Mutex};
+use std::fmt::Display;
+
+struct DebounceData<A, B: FnMut(A) + Send + Sync + 'static> {
+    value: A,
+    timer: Timer,
+    timer_guard: Option<Guard>,
+    callback: B,
+}
+
+
+pub fn closure<A: Display + Eq + Copy + Send + Sync + 'static, B: FnMut(A) + Send + Sync + 'static>(default_value: A, f: B) -> Box<Fn(A) -> () + Send + Sync> {
+    let bounce_time: Duration = Duration::milliseconds(10);
+    let state = Arc::new(Mutex::new(DebounceData {
+        value: default_value,
+        timer: Timer::new(),
+        timer_guard: None,
+        callback: f,
+    }));
+    return Box::new(move |new_value| {
+        let mut data = state.lock().unwrap();
+        if data.value != new_value {
+            if new_value != default_value {
+                data.value = new_value;
+                match &mut data.timer_guard {
+                    Some(guard) => drop(guard),
+                    None => (&mut data.callback)(new_value),
+                }
+                data.timer_guard = None;
+            } else {
+                data.value = new_value;
+                let state_ref = state.clone();
+                let guard = data.timer.schedule_with_delay(bounce_time, move || {
+                    let mut data = state_ref.lock().unwrap();
+                    data.timer_guard = None;
+                    (&mut data.callback)(new_value);
+                });
+                data.timer_guard = Some(guard);
+            }
+        }
+    })
+}
--- src/main.rs
+++ src/main.rs
@@ -4,8 +4,11 @@ use std::thread;
 use std::io::{stdin,stdout,Write};
 use std::error::Error;
 mod coffeepot;
+mod debounce;
 use coffeepot::Coffeepot;
 use chrono::prelude::*;
+use rppal::gpio::Gpio;
+use rppal::gpio::Level;
 
 /*fn cycle_pwm() -> Result<(), Box<dyn Error>> {
 	let led = Pwm::with_period(Channel::Pwm0, Duration::from_millis(5), Duration::from_millis(2), Polarity::Normal, true)?;
@@ -20,9 +23,36 @@ use chrono::prelude::*;
 	Ok(())
 }*/
 
-fn main() -> Result<(), Box<dyn Error>> {
+fn demo() -> Result<(), Box<dyn Error>> {
 	println!("Hello, world!");
         let coffeepot = Coffeepot::new(&|new_state| println!("state changed to {:?}", new_state));
+        loop {
+            // Need empty string at start of every loop iteration, read_line appends
+            let mut input = String::new();
+            stdin().read_line(&mut input).expect("Did not enter a correct string");
+            match input.trim().as_ref() {
+                "a" => coffeepot.activate(chrono::Duration::seconds(2)),
+                "i" => coffeepot.inactivate(),
+                "r" => coffeepot.toggle_ready(),
+                "d" => coffeepot.activate_delayed(chrono::Duration::seconds(5), Local::now() + FixedOffset::east(5)),
+                other => println!("unexpected input: {}", other),
+            }
+            println!("state: {:?}", coffeepot.current_state());
+        }
+        Ok(())
+}
+
+// Gpio uses BCM pin numbering
+const GPIO_READY_PIN: u8 = 17;
+
+
+fn main() -> Result<(), Box<dyn Error>> {
+	println!("Hello, world!");
+        let mut ready_input = Gpio::new()?.get(GPIO_READY_PIN)?.into_input_pulldown();
+        let coffeepot = Coffeepot::new(&|new_state| println!("state changed to {:?}", new_state));
+        let pot2 = coffeepot.clone();
+        let update_func = debounce::closure(Level::Low, move |level| { println!("level: {}", level); pot2.toggle_ready();});
+        ready_input.set_async_interrupt(rppal::gpio::Trigger::Both, update_func)?;
         loop {
             // Need empty string at start of every loop iteration, read_line appends
             let mut input = String::new();
